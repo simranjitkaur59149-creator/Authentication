b40a088b8c32706a295254aa5d4b24483221b24d
simran <simranjitkaur59149@gamil.com>
2026-03-10 11:45:42 +0530

    authentication project

---
+++ auth.js
@@ -0,0 +1,62 @@
+import express from "express";
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const app = express();
+const PORT = 8000;
+const saltRound = 12;
+app.use(express.json());
+mongoose
+  .connect("mongodb://127.0.0.1:27017/usersDB")
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, required: true, match: /.+\@.+\..+/, unique: true },
+  password: String,
+  created: { type: Date, default: Date.now() },
+  updated: { type: Date, default: Date.now() },
+});
+userSchema.pre("updateOne", (next) => {
+  console.log("running");
+  this.set({ updated: Date.now() });
+  next();
+});
+const User = mongoose.model("User", userSchema);
+app.post("/addusers", async (req, res) => {
+  try {
+    const password = req.body.password;
+    const hashpassword = await bcrypt.hash(password, saltRound);
+    req.body.password = hashpassword;
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+app.get("/users", async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 3;
+    const skip = (page - 1) * limit;
+
+    const user = await User.find().skip(skip).limit(limit);
+    const userData = user.map((data) => {
+      return {
+        name: data.name,
+        email: data.email,
+        created: data.created,
+        updated: data.updated,
+      };
+    });
+    const totalusers = await User.countDocuments();
+    const totalPages = Math.ceil(totalusers / limit);
+    res
+      .status(200)
+      .json({ userData, totalusers, totalPages, currentPage: page });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
---
+++ index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+const PORT = 8000;
+app.use(express.json());
+//establish connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/crud")
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
+//schema
+const userSchema = new mongoose.Schema({
+  name: { type: String, require: true },
+  email: { type: String, require: true, match: /.+\@.+\..+/ ,unique:true},
+
+});
+//Model compiled from schema
+const User = mongoose.model("User", userSchema);
+//CRUD operations
+//create users
+app.post("/users", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+//get all users
+app.get("/users", async (req, res) => {
+  try {
+    const page=parseInt(req.query.page)||1 //by default page=1
+    const limit=parseInt(req.query.limit)||4 //by default limit=4
+    const skip=(page-1)*limit
+    const totalUsers= await User.countDocuments()
+    const user = await User.find().skip(skip).limit(limit);
+    res.status(200).json({totalUsers,totalPages:Math.ceil(totalUsers/limit),currentpage:page,user});
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+//get one user by id
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: "Invaild ID" });
+  }
+});
+//update one user by id
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user) return res.status(404).json({ error: "User not found" });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
